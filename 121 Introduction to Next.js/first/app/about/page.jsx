cc06d43a25564a75a2d28a5b666bc4cfba434862
Debapriyo Das <debapriyo00@gmail.com>
2024-04-04 17:06:42 +0530

    Understanding Next.js File Structure

---
+++ 121 Introduction to Next.js/first/app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className='text-xl text-center my-8 font-bold'> Problem Solved by Next.js</h1>
+        <ul className='flex flex-col items-center text-teal-400'>
+            <li>Full Stack Solution</li>
+            <li>File base Routing</li>
+            <li>Addition feature like Router from Next/Navigation</li>
+            <li>Optimized rendering</li>
+        </ul>
+    </div>
+  )
+}
+
+export default page
